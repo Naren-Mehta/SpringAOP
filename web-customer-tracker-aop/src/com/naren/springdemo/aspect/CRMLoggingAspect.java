44b98b60963cdece96634d2afd8d5e4e8fe8961f
Naren-Mehta <deepakmehta.singh@gmail.com>
2019-08-19 23:31:10 +0530

    Working on web application with aop

--- web-customer-tracker-aop/src/com/naren/springdemo/aspect/CRMLoggingAspect.java
+++ web-customer-tracker-aop/src/com/naren/springdemo/aspect/CRMLoggingAspect.java
@@ -2,9 +2,13 @@ package com.naren.springdemo.aspect;
 
 import java.util.logging.Logger;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+import org.springframework.ui.context.Theme;
 
 @Aspect
 @Component
@@ -12,25 +16,42 @@ public class CRMLoggingAspect {
 
 	private Logger myLogger= Logger.getLogger(getClass().getName());
 	
-	@Pointcut("execution(* com.naren.springdemo.aspect.controller.*.*(..))")
+	@Pointcut("execution(* com.naren.springdemo.controller.*.*(..))")
 	public void forControllerPackage() {
 		
 	}
 	
 	
-	@Pointcut("execution(* com.naren.springdemo.aspect.service.*.*(..))")
+	@Pointcut("execution(* com.naren.springdemo.service.*.*(..))")
 	public void forServicePackage() {
 		
 	}
 	
-	@Pointcut("execution(* com.naren.springdemo.aspect.dao.*.*(..))")
+	@Pointcut("execution(* com.naren.springdemo.dao.*.*(..))")
 	public void forDaoPackage() {
 		
 	}
 	
-	@Pointcut("")
-	public void forDaoPackage() {
+	@Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+	public void forAppFlow() {
 		
+	}
+	
+	@Before("forAppFlow()")
+	public void before(JoinPoint theJoinPoint) {
+		String methodString= theJoinPoint.getSignature().toShortString();
+		myLogger.info("===>> in @Before: calling method: " +methodString);
+	}
+	
+	@AfterReturning(
+			pointcut = "forAppFlow()",
+			returning = "result"
+			)
+	public void afterReturning(JoinPoint theJoinPoint, Object result) {
+		String methodString= theJoinPoint.getSignature().toShortString();
+		myLogger.info("===>> in @AfterReturning: calling method: " +methodString);
+		myLogger.info("===>> result: " +result);
+
 	}
 	
 }
